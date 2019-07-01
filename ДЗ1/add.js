322f53e1e929204dcb45303e6e96dafbd3eae98a
Danulichkak <52253339+Danulichkak@users.noreply.github.com>
2019-07-01 15:17:11 +0300

    Add files via upload

---
+++ ДЗ1/add.js
@@ -0,0 +1,21 @@
+console.log( '<H1>Ты съел конфет:</H1>' )
+var num=0; 
+while ( num != null ){
+    num=prompt ('Сколько конфет ты съел?', '')
+    if (num!=null) {
+        console.log( num + 'шт;' );
+        num = num.replace(/,/g, "." );
+        if (isNaN(num)) {
+            console.log( 'Количесвто только числом!' + '<br>' );
+        }
+        else if (num<0) {
+            console.log( 'Не ври! Я знаю что ты их ел!' + '<br>' );
+        }
+        else if (num>70) {
+            console.log( 'Ты не человек! Столько за раз не едят!' + '<br>' );
+        }
+        else if (num==0) {
+            console.log( 'Да не может быть!?!?!?' + '<br>' );
+        }
+    }
+}

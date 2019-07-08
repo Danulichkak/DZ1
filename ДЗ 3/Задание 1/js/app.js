24ce5b326f27078d4a308e60b3fa9ca0cab9a3e7
Danulichkak <52253339+Danulichkak@users.noreply.github.com>
2019-07-08 18:14:11 +0300

    Add files via upload
    Измененное

--- ДЗ 3/Задание 1/js/app.js
+++ ДЗ 3/Задание 1/js/app.js
@@ -1,3 +1,21 @@
+var someElts = ['1', '5', 'bar', 'undefined', '6', '1', 'bar', 'undefined', 1, 1, 1,  '7', '6'];
+function elArr (arr){
+	var result =[];
+	for (var item of arr){
+		if (result.indexOf(item)<0){
+			result.push(item);
+		}
+	}
+	return result;
+}
+elArr(someElts);
+
+
+/*  Если добавить элементы в вар то сработает не правильно ( элементы 1 будет сравнивать не обращая внимания на тип что является ошибкой)
+var someElts = ['1', '5', 'bar', 'undefined', '6', '1', 'bar', 'undefined', 1, 1, 1,  '7', '6'];
+
+
+Вариант №1
 var someElts = ['1', '5', 'bar', 'undefined', '6', '1', 'bar', 'undefined', '7', '6'];
 
 
@@ -8,6 +26,21 @@ var newElements = someElts.sort().reduce(function(someElts, el){
 	return someElts;
 }, []);
 
-console.log( newElements );
+console.log( newElements ); */
 
 
+
+
+
+/* Вариант №2
+var someElts = ['1', '5', 'bar', 'undefined', '6', '1', 'bar', 'undefined', 1, 1, 1,  '7', '6'];
+
+
+var newElements = someElts.sort().reduce(function(someElts, el){
+	if(!someElts.length || someElts.length && someElts[someElts.length - 1] !== el) {
+		someElts.push(el);
+	}
+	return someElts;
+}, []);
+
+console.log( newElements ); */

c4c9cb1940bd58d621572294a8595d34afcd8d6c
Danulichkak <52253339+Danulichkak@users.noreply.github.com>
2019-07-22 13:49:36 +0300

    Add files via upload

---
+++ ДЗ 8/js/app.js
@@ -0,0 +1,27 @@
+var contrTable = (contriesText) => {
+    let resultStr = '<table border="1"><thead><tr>' +
+        '<td>Название</td><td>Столица</td><td>Кол-во население</td><td>Площадь</td><td>Валюты</td><td>Языки</td><td>Флаг</td><td>Соседи</td>' +
+        '</tr></thead></thead><tbody>';
+    for(let element of contriesText) {
+        resultStr += `<tr><td>${element.name}</td><td>${element.capital}</td><td>${element.population}</td><td>${element.area}</td><td>${element.currencies}</td><td>${element.languages}</td>
+<td><img src="'${element.flag}'"></td><td>${element.border}</td></tr>`;
+        
+        
+    }
+    resultStr += '</tbody></table>';
+    $('.contries').html(resultStr);
+};
+
+$(document).ready(() => {
+    $('.btn').click(() => {
+        $.ajax({
+            url: "https://restcountries.eu/rest/v2/all",
+            method: 'GET'
+        }).done((contriesText) => {
+            console.log(contriesText);
+            contrTable(contriesText);
+        });
+    })
+});
+
+/*<td><img src="${element.flag}>*/

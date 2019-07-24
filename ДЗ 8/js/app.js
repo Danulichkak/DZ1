3002a66d2e49c8162d548c1a4b99dfd573d71741
Danulichkak <52253339+Danulichkak@users.noreply.github.com>
2019-07-24 13:09:25 +0300

    Add files via upload

--- ДЗ 8/js/app.js
+++ ДЗ 8/js/app.js
@@ -3,10 +3,27 @@ var contrTable = (contriesText) => {
         '<td>Название</td><td>Столица</td><td>Кол-во население</td><td>Площадь</td><td>Валюты</td><td>Языки</td><td>Флаг</td><td>Соседи</td>' +
         '</tr></thead></thead><tbody>';
     for(let element of contriesText) {
-        resultStr += `<tr><td>${element.name}</td><td>${element.capital}</td><td>${element.population}</td><td>${element.area}</td><td>${element.currencies}</td><td>${element.languages}</td>
-<td><img src="${element.flag}"></td><td>${element.borders}</td></tr>`;
-      
-    }
+        resultStr += `<tr><td>${element.name}</td><td>${element.capital}</td><td>${element.population}</td><td>${element.area}</td>`;
+        let currencies = element.currencies.map((item) => {
+            return item.name;
+        });
+        resultStr += `${currencies.join(', ')}</td><td>`;
+
+        let languages = element.languages.map((item) => {
+            return item.name;
+        });
+        resultStr += `${languages.join(', ')}</td><td><img src="${element.flag}"></td><td>`;
+        let borderNames = [];
+        for (let borderItem of element.borders) {
+            for (let searchArray in contriesText) {
+                if (contriesText[searchArray].alpha3Code === borderItem) {
+                    borderNames.push(contriesText[searchArray].name);
+                }
+            }
+        }
+
+        resultStr += `${borderNames.join(', ')}</td></tr>`;        
+    };
     resultStr += '</tbody></table>';
     $('.contries').html(resultStr);
 };
@@ -22,6 +39,5 @@ $(document).ready(() => {
         });
     })
 });
-
 
 

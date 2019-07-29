37c6f0299fa2c93a6517bc019dbceed52a60cd98
Danulichkak <52253339+Danulichkak@users.noreply.github.com>
2019-07-29 14:11:16 +0300

    Add files via upload

--- ДЗ 9/js/app.js
+++ ДЗ 9/js/app.js
@@ -1,9 +1,11 @@
-var contrTable = (contriesText) => {
-    let resultStr = '<table border="1"><thead><tr>' +
-        '<td>Название</td><td>Столица</td><td>Кол-во население</td><td>Площадь</td><td>Валюты</td><td>Языки</td><td>Флаг</td><td>Соседи</td>' +
-        '</tr></thead></thead><tbody>';
-    for(let element of contriesText) {
-        resultStr += `<tr><td>${element.name}</td><td>${element.capital}</td><td>${element.population}</td><td>${element.area}</td><td>`;
+function contrTable(contriesText,valContry) {
+    let resultStr = '<table class="table" border = "1"><thead><tr><th>#</th><td>Название</td><td>Столица</td><td>Кол-во население</td><td>Площадь</td><td>Валюты</td><td>Языки</td><td>Флаг</td><td>Соседи</td></tr></thead></thead><tbody>';
+    for (let index in contriesText) {
+        let element = contriesText[index];
+        if(valContry && element.name !== valContry) {
+            continue;
+        }
+        resultStr += `<tr><td>${+index+1}</td><td class="name_country">${element.name}</td><td>${element.capital}</td><td>${element.population}</td><td>${element.area}</td><td>`;
         let currencies = element.currencies.map((item) => {
             return item.name;
         });
@@ -22,16 +24,16 @@ var contrTable = (contriesText) => {
             }
         }
 
-        resultStr += `${borderNames.join(', ')}</td></tr>`;        
+        resultStr += `${borderNames.join(', ')}</td></tr>`;
     };
     resultStr += '</tbody></table>';
-
+    
     let countriesNames = contriesText.map((el) => {
         return el.name;
     });
 
-    if(!newValue) {
-        $( "#contrTags" ).autocomplete({
+    if(!valContry) {
+        $( "#nameContries" ).autocomplete({
             source: countriesNames,
             select: function(event, ui) {
                 contrTable(contriesText, ui.item.value);
@@ -39,19 +41,34 @@ var contrTable = (contriesText) => {
         });
     }
 
+    /* $(document).ready(function () {
+        $("#nameContries").keyup(function () {
+            _this = this;
+            $.each($("#table tbody tr"), function () {
+                if ($(this).text().toLowerCase() === -1) {
+                    $(this).hide();
+                } else {
+                    $(this).show();
+                };
+            });
+        });
+    }); */
 
 
     $('.contries').html(resultStr);
 };
 
 $(document).ready(() => {
+    $('.ui-widget').hide();
+
     $('.btn').click(() => {
         $.ajax({
-            url: "https://restcountries.eu/rest/v2/all",
-            method: 'GET'
-        }).done((contriesText) => {
-            console.log(contriesText);
-            contrTable(contriesText);
+            url: "https://restcountries.eu/rest/v2/all"
+        }).done((data) => {
+            $( "#nameContries").val();
+            $('.ui-widget').show();
+            $('.btn').hide();
+            contrTable(data);
         });
     })
 });
